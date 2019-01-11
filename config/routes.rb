d25fd9fab54bc975ce7022764e4063aabddf60c9
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-11 16:14:53 -0500

    signup added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get  'about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
