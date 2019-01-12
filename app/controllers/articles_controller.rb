98447a02b753cab875edb1def3bbf162063d6eef
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-11 23:36:49 -0500

    Complete auth system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,58 +1,62 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
-
-
+  
   def new
     @article = Article.new
-  end 
-
-  def edit 
-    @article = Article.find(params[:id])
   end
-
-  def create 
+  
+  def edit
+  end
+  
+  def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.find(5)
     if @article.save
-      flash[:success] = "Article was successfully created"
+      flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
-    else 
+    else
       render 'new'
     end
   end
-
+  
   def update
-    @article = Article.find(params[:id])
-      if @article.update(article_params)
-        flash[:success] = "Article was successfully updated"
-        redirect_to article_path(@article)
-      else
-        render 'edit'
-      end
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
-
-  def show
-    @article = Article.find(params[:id])
   end
-
-  def destroy 
-    @article = Article.find(params[:id])
+  
+  def show
+  end
+  
+  def destroy
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
-  end 
-
-
-  private 
+  end
+  
+  private
+  
   def set_article
     @article = Article.find(params[:id])
-  end 
-
+  end
+  
   def article_params
-    params.require(:article).permit(:title,:description)
+    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
   end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root 'pages#home'
-  get  'about', to: 'pages#about'
-
+  get 'about', to: 'pages#about'
+  
   resources :articles
 
-  get 'signup', to: 'users#new'
-
+  get 'signup', to: "users#new"
   resources :users, except: [:new]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
 end
