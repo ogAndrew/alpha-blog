cbd5c22d614dcd43bc7586c02c0fceb6d85b2ad3
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-14 18:41:05 -0500

    add association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord 
   belongs_to :user 
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
   validates :description, presence: true, length: { minimum: 3, maximum: 200}
   validates :user_id, presence: true 
-
 end 
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -12,8 +16,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_match "Sports", response.body
   end
-
+  
   test "invalid form submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
@@ -23,4 +28,4 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
-end 
+end
