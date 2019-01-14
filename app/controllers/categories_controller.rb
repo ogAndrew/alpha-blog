e738228b90de729b5a93eaf8578e5062d8a90bb1
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-14 17:31:37 -0500

    admin test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,32 +1,41 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
-  end 
-
-  def new 
-    @category = Category.new    
-  end 
-
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
   def create
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = "Category was created successfully"
       redirect_to categories_path
-    else 
+    else
       render 'new'
-    end 
-  end 
-
-  def show 
-  end 
-
+    end
+  end
+  
+  def show
+    
+  end
+  
   private
-
+  
   def category_params
     params.require(:category).permit(:name)
-  end 
-
-end 
+  end
+  
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
+  
+end
 
   
